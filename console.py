cdb7004c34309ca8b04b2723da8151965a01b1e4
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-10-23 12:18:11 -0700

    large task

--- console.py
+++ console.py
@@ -236,12 +236,13 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
+            dicti = storage.all(args)
+            for k, v in dicti.items():
                 print_list.append(str(v))
+        else:
+            dicti = storage.all()
+            for k, v in dicti.items():
+                print_list.appendi(str(v))
 
         print(print_list)
 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import os
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="State", cascade="all,delete")
+    else:
+        @property
+        def cities(self):
+            """
+            returns the list of City instances with 
+            state_id equals to the current State.id
+            """
+            cit = []
+            list_of_all = models.storage.all("City")
+            for city in list_of_all:
+                if list_of_all[city].state_id == self.id:
+                    cit.append(list_of_all[city])
+            return cit
+
+
+

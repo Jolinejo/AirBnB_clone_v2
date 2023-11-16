ac47d68df6c5f9b8ba03e7dced8ffb3047f7e88c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-16 13:38:53 -0800

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from fabric.api import local, run, env, put
 from os.path import exists
 env.hosts = ['54.236.47.113', '100.25.170.6']
 
+
 def do_deploy(archive_path):
     """deploy the archive"""
     if exists(archive_path) is False:
@@ -14,14 +15,15 @@ def do_deploy(archive_path):
     try:
         name = archive_path.split('/')[-1]
         put(archive_path, '/tmp/{}'.format(name))
-        run('mkdir -p /data/web_static/releases/{}/'.format(name.split('.')[0]))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(name, name.split('.')[0]))
-        run('rm /tmp/{}'.format(name))
         spl = name.split('.')[0]
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(spl, spl))
+        run('mkdir -p /data/web_static/releases/{}/'.format(name.spl))
+        dist = "/data/web_static/releases/"
+        run('tar -xzf /tmp/{} -C {}{}/'.format(name, dist, spl))
+        run('rm /tmp/{}'.format(name))
+        run('mv {}{}/web_static/* {}{}/'.format(dist, spl, dist, spl))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(spl))
         run('rm -rf /data/web_static/current')
-        run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(name.split('.')[0]))
+        run('ln -sf {}{}/ /data/web_static/current'.format(dist, spl))
         return True
-    except:
+    except Exception:
         return False

3f65fad5f2ad25a87e95d91f49c3471fa0cf65e5
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-16 09:06:37 -0800

    task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+create file archive
+"""
+import datetime
+from fabric.api import local
+
+def do_pack():
+    """
+    web_static_<year><month><day><hour><minute><second>.tgz
+    """
+    try:
+        now = datetime.datetime.now()
+        now = now.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        name = "versions/web_static_{}".format(now)
+        local("tar -cvzf {} web_static".format(name))
+        return name
+    except Exception:
+        return None

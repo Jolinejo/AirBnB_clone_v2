003133bec2f7919e13370cad22447ca286a17f18
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-16 22:45:59 -0800

    task

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,18 +28,16 @@ def do_deploy(archive_path):
     try:
         name = archive_path.split('/')[-1]
         put(archive_path, '/tmp/{}'.format(name))
-        spl = name.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(spl))
-        dis = '/data/web_static/releases/{}/'.format(spl)
-        c = 'tar -xzf'
-        run('{} /tmp/{} -C /data/web_static/releases/{}/'.format(c, name, spl))
+        run('mkdir -p /data/web_static/releases/{}/'.format(name.split('.')[0]))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(name, name.split('.')[0]))
         run('rm /tmp/{}'.format(name))
-        run('mv /data/web_static/releases/{}/web_static/* {}'.format(spl, dis))
+        spl = name.split('.')[0]
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(spl, spl))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(spl))
         run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dis))
+        run('ln -f /data/web_static/releases/{}/ /data/web_static/current'.format(name.split('.')[0]))
         return True
-    except Exception:
+    except:
         return False
 
 

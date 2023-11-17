441085272110920ebe6ba3b4e2b4af97ec12806c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-17 04:07:52 -0800

    task

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,7 @@
 import datetime
 from fabric.api import local, run, env, put
 from os.path import exists
-env.hosts = ['54.236.47.113', '100.25.170.6']
+env.hosts = ['54.88.43.0', '18.235.249.83']
 
 
 def do_pack():
@@ -35,7 +35,7 @@ def do_deploy(archive_path):
         run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(spl, spl))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(spl))
         run('rm -rf /data/web_static/current')
-        run('ln -f /data/web_static/releases/{}/ /data/web_static/current'.format(name.split('.')[0]))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name.split('.')[0]))
         return True
     except:
         return False

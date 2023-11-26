0b2b95276fa34258d58735e51f5f8f9f829cc833
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-26 14:36:30 -0800

    task

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -83,3 +83,7 @@ class DBStorage():
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(self.__engine, expire_on_commit=True)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """call remove() method on the session attribute"""
+        self.__session.remove()

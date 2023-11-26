876ede8ef3b3d081f6cdb6a88a472e534af88923
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2023-11-26 13:01:57 -0800

    task

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+start flask module
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """
+    returning hello hbnb
+    """
+    return "Hello HBNB!"
+
+
+@app.rout('/hbnb', strict_slashes=False)
+def main():
+    """
+    returning hello hbnb
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
